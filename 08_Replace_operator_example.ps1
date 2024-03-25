974492e1e8d95132ff7497cb2a9f9378f9b6e89e
ShahbazSayy3d <141297247+ShahbazSayy3d@users.noreply.github.com>
2024-03-25 23:44:13 +0530

    Add files via upload
    Two examples of Replace

---
+++ 08_Replace_operator_example.ps1
@@ -0,0 +1,12 @@
+﻿#using replace operator in a string(example 1)
+$str = "Hello World!!!"
+$str -replace "hello" ,"Hii"
+$str
+
+<#
+#using replace method on a string(Example 2)
+$string = "Welcome to my Home"
+$string = $string.Replace('to' , 'on')
+$string
+
+#>
