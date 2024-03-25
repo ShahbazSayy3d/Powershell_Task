0830e159779530ef285da317f739eb20ec25adbb
ShahbazSayy3d <141297247+ShahbazSayy3d@users.noreply.github.com>
2024-03-25 19:37:21 +0530

    Add files via upload

---
+++ 06_list_the_dat_files_from_windows.ps1
@@ -0,0 +1,32 @@
+﻿#to get the .dat file from the windows folder
+#$list = 
+Get-ChildItem -Path C:\Windows\*.dat | Format-Table -Property Name, CreationTime, Length
+
+
+
+#to get the .dat file from its sub directories
+$newList = Get-ChildItem -Path C:\Windows\.. -Recurse -File "*.dat" | Format-Table -Property Name, CreationTime, Length
+#ConvertTo-Csv -InputObject 
+$newList 
+
+#| New-Item -destination C:\Users\DELL\Favorites
+
+
+
+
+
+
+
+
+
+
+<#
+$new_list = Get-ChildItem -Path C:\Windows\
+
+foreach ($items in $new_list){
+   #Get-Item -Filter "*.dat"
+   Format-Table -InputObject $new_list
+}
+Format-Table -InputObject $list
+Format-Table -InputObject $newList 
+#>
---
+++ 07_Split_operator_example.ps1
@@ -0,0 +1,10 @@
+﻿#Making a variable and using split(Example 1)
+<#
+$name = -split "Shahbaz Sayyed"
+$name 
+#>
+
+
+#taking Input from the user and performing split operation(Example 2)
+$name = Read-Host "Enter your full name" 
+-split $name 
